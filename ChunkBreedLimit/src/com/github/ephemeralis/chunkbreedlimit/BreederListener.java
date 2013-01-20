c06a00a51b06fe8cd13396411bd4f2f9536dfe09
computerxpds <dstrickner@gmail.com>
2013-01-20 16:39:54 -0500

    trying to reorganize some things..

--- ChunkBreedLimit/src/com/github/ephemeralis/chunkbreedlimit/BreederListener.java
+++ ChunkBreedLimit/src/com/github/ephemeralis/chunkbreedlimit/BreederListener.java
@@ -15,162 +15,140 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class BreederListener implements Listener {
-	
+
 	private ChunkBreedLimit basePlugin;
 	private int spawnCap;
 	private List<EntityType> allowedEntities;
 	private Hashtable<EntityType, Integer> individualCapData;
 	private String breedingFailMessage;
 	private boolean individualEntityCap;
-	
-	public BreederListener(ChunkBreedLimit plugin, int cap, List<EntityType> allowedEnts, String bmsg)
-	{
-		//no individual entity counting
+
+	public BreederListener(ChunkBreedLimit plugin, int cap,
+			List<EntityType> allowedEnts, String bmsg) {
+		// no individual entity counting
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 		spawnCap = cap;
 		allowedEntities = allowedEnts;
 		breedingFailMessage = bmsg;
 		individualEntityCap = false;
 	}
-	
-	public BreederListener(ChunkBreedLimit plugin, int cap, Hashtable<EntityType, Integer> capdata, String bmsg)
-	{
-		//individual entity counting
+
+	public BreederListener(ChunkBreedLimit plugin, int cap,
+			Hashtable<EntityType, Integer> capdata, String bmsg) {
+		// individual entity counting
 		basePlugin = plugin;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 		individualCapData = capdata;
 		breedingFailMessage = bmsg;
 		allowedEntities = new ArrayList<EntityType>();
 		individualEntityCap = true;
-		
+
 		Enumeration<EntityType> entityTypes = capdata.keys();
-		
-		while (entityTypes.hasMoreElements())
-		{
+
+		while (entityTypes.hasMoreElements()) {
 			EntityType temp = entityTypes.nextElement();
 			allowedEntities.add(temp);
 		}
-		
-		if (allowedEntities.size() == 0)
-		{
+
+		if (allowedEntities.size() == 0) {
 			plugin.getLogger().info("Entity list is length 0!");
 		}
 
 	}
-	
+
 	@EventHandler
-	public void onCreatureSpawnEvent(CreatureSpawnEvent event)
-	{
-		if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.BREEDING 
+	public void onCreatureSpawnEvent(CreatureSpawnEvent event) {
+		if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.BREEDING
 				|| event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.EGG
-				|| event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.SPAWNER_EGG)
-		{
-			
-			if (individualEntityCap && allowedEntities.contains(event.getEntityType()))
-			{
-				event.setCancelled(checkAndApplyEntityWithCap(event.getEntityType(), 
-						event.getLocation().getChunk(),
+				|| event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.SPAWNER_EGG) {
+
+			if (individualEntityCap
+					&& allowedEntities.contains(event.getEntityType())) {
+				event.setCancelled(checkAndApplyEntityWithCap(
+						event.getEntityType(), event.getLocation().getChunk(),
 						individualCapData.get(event.getEntityType())));
-			}
-			else
-			{
-				//otherwise, just use standard checking
-				event.setCancelled(checkAndApplyEntity(event.getEntityType(), 
+			} else {
+				// otherwise, just use standard checking
+				event.setCancelled(checkAndApplyEntity(event.getEntityType(),
 						event.getLocation().getChunk()));
 			}
-			
-		} 
+
+		}
 	}
-	
-	public boolean checkAndApplyEntityWithCap(EntityType ent, Chunk chunk, Integer cap)
-	{
+
+	public boolean checkAndApplyEntityWithCap(EntityType ent, Chunk chunk,
+			Integer cap) {
 		/*
 		 * Checks and applies entity cap using individual cap limits
 		 */
 		Entity[] entitiesInEventChunk = chunk.getEntities();
-		
+
 		Integer entcount = 0;
-		for (Entity entt : entitiesInEventChunk)
-		{
-			//we have entity list, so iterate through it
-			//to determine just how many of the same type
-			//we have on the same chunk
-			if (entt.getType() == ent)
-			{
-				//same type detected, so increase
+		for (Entity entt : entitiesInEventChunk) {
+			// we have entity list, so iterate through it
+			// to determine just how many of the same type
+			// we have on the same chunk
+			if (entt.getType() == ent) {
+				// same type detected, so increase
 				entcount++;
 			}
 		}
-		
-		if (entcount >= cap)
-		{
+
+		if (entcount >= cap) {
 			return true;
-		} 
-		else
-		{
+		} else {
 			return false;
 		}
 	}
-	
-	public boolean checkAndApplyEntity(EntityType ent, Chunk chunk)
-	{
+
+	public boolean checkAndApplyEntity(EntityType ent, Chunk chunk) {
 		/*
 		 * Checks and applies entity cap using global cap limit
 		 */
 		Entity[] entitiesInEventChunk = chunk.getEntities();
-		
+
 		int entcount = 0;
-		for (Entity entt : entitiesInEventChunk)
-		{
-			//we have entity list, so iterate through it
-			//to determine just how many of the same type
-			//we have on the same chunk
-			if (entt.getType() == ent)
-			{
-				//same type detected, so increase
+		for (Entity entt : entitiesInEventChunk) {
+			// we have entity list, so iterate through it
+			// to determine just how many of the same type
+			// we have on the same chunk
+			if (entt.getType() == ent) {
+				// same type detected, so increase
 				entcount++;
 			}
 		}
-		
-		if (entcount >= spawnCap)
-		{
+
+		if (entcount >= spawnCap) {
 			return true;
-		} 
-		else
-		{
+		} else {
 			return false;
 		}
 	}
-	
+
 	@EventHandler
-	public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent event)
-	{
+	public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent event) {
 		Player p = event.getPlayer();
-		
-		if ((p.getItemInHand().getType() == Material.WHEAT ||
-				p.getItemInHand().getType() == Material.RAW_BEEF ||
-				p.getItemInHand().getType() == Material.PORK ||
-				p.getItemInHand().getType() == Material.COOKED_BEEF ||
-				p.getItemInHand().getType() == Material.RAW_FISH)
-				&& allowedEntities.contains(event.getRightClicked().getType()))
-		{
+
+		if ((p.getItemInHand().getType() == Material.WHEAT
+				|| p.getItemInHand().getType() == Material.RAW_BEEF
+				|| p.getItemInHand().getType() == Material.PORK
+				|| p.getItemInHand().getType() == Material.COOKED_BEEF || p
+				.getItemInHand().getType() == Material.RAW_FISH)
+				&& allowedEntities.contains(event.getRightClicked().getType())) {
 			int entcount = 0;
-			//player is holding wheat and the entity is allowed, check
-			for (Entity ent : event.getRightClicked().getLocation().getChunk().getEntities())
-			{
+			// player is holding wheat and the entity is allowed, check
+			for (Entity ent : event.getRightClicked().getLocation().getChunk()
+					.getEntities()) {
 				if (allowedEntities.contains(ent.getType()))
 					entcount++;
 			}
-			
-			if (entcount >= spawnCap)
-			{
+
+			if (entcount >= spawnCap) {
 				p.sendMessage(ChatColor.RED + breedingFailMessage);
-				event.setCancelled(true);	
+				event.setCancelled(true);
 			}
 		}
-		
+
 	}
-	
+
 }
-
-
--- ChunkBreedLimit/src/com/github/ephemeralis/chunkbreedlimit/ChunkBreedLimit.java
+++ ChunkBreedLimit/src/com/github/ephemeralis/chunkbreedlimit/ChunkBreedLimit.java
@@ -10,78 +10,82 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChunkBreedLimit extends JavaPlugin {
-	
+
 	public int entitySpawnCap;
 	public List<EntityType> allowedEntityList = new ArrayList<EntityType>();
 	public Hashtable<EntityType, Integer> individCapData = new Hashtable<EntityType, Integer>();
-	
+
 	@Override
-	public void onEnable()
-	{
+	public void onEnable() {
 		this.saveDefaultConfig();
 		entitySpawnCap = this.getConfig().getInt("spawn-cap", 100);
-		boolean individualCap = this.getConfig().getBoolean("use-individual-cap", false);
-		String breedingFailMessage = this.getConfig().getString("breeding-fail-msg", "The animal cannot breed - it is too crowded!");
-		
-		if (individualCap)
-		{
-			List<String> entityListLoad = this.getConfig().getStringList("entity-list-individual");
-			if (entityListLoad.size() == 0)
-			{
-				getLogger().info("Entity list is invalid for individual cap loading! Reverting to defaults..");
-				
+		boolean individualCap = this.getConfig().getBoolean(
+				"use-individual-cap", false);
+		String breedingFailMessage = this.getConfig().getString(
+				"breeding-fail-msg",
+				"The animal cannot breed - it is too crowded!");
+
+		if (individualCap) {
+			List<String> entityListLoad = this.getConfig().getStringList(
+					"entity-list-individual");
+			if (entityListLoad.size() == 0) {
+				getLogger()
+						.info("Entity list is invalid for individual cap loading! Reverting to defaults..");
+
 				individCapData.put(EntityType.COW, 100);
 				individCapData.put(EntityType.CHICKEN, 100);
 				individCapData.put(EntityType.PIG, 100);
 				individCapData.put(EntityType.SHEEP, 100);
-			}
-			else
-			{
-				for (String entry : entityListLoad)
-				{
+			} else {
+				for (String entry : entityListLoad) {
 					String[] temp = entry.split(",");
 					int tempint = Integer.parseInt(temp[1].trim());
-					EntityType temptype = EntityType.fromName(temp[0].toUpperCase());
-					getLogger().info(String.format("%s, %s", temptype, tempint));
+					EntityType temptype = EntityType.fromName(temp[0]
+							.toUpperCase());
+					getLogger()
+							.info(String.format("%s, %s", temptype, tempint));
 					individCapData.put(temptype, tempint);
 				}
 			}
-			
-			new BreederListener(this, entitySpawnCap, individCapData, breedingFailMessage);
+
+			new BreederListener(this, entitySpawnCap, individCapData,
+					breedingFailMessage);
 			getLogger().info("Watching individual entity limits");
-		}
-		else
-		{
-			List<String> entityListLoad = this.getConfig().getStringList("entity-list");
-			if (entityListLoad.size() == 0)
-			{
-				getLogger().info("Entity list appears invalid! Reverting to defaults..");
-				
+		} else {
+			List<String> entityListLoad = this.getConfig().getStringList(
+					"entity-list");
+			if (entityListLoad.size() == 0) {
+				getLogger().info(
+						"Entity list appears invalid! Reverting to defaults..");
+
 				allowedEntityList.add(EntityType.COW);
 				allowedEntityList.add(EntityType.CHICKEN);
 				allowedEntityList.add(EntityType.PIG);
 				allowedEntityList.add(EntityType.SHEEP);
 			} else {
-				//load list from config
-				
+				// load list from config
+
 				getLogger().info("Loading breeding deny list from config..");
-				
-				for (String ent : entityListLoad)
-				{
-						EntityType temp = EntityType.fromName(ent);
-						allowedEntityList.add(temp);
+
+				for (String ent : entityListLoad) {
+					EntityType temp = EntityType.fromName(ent);
+					allowedEntityList.add(temp);
 				}
 			}
-			
-			new BreederListener(this, entitySpawnCap, allowedEntityList, breedingFailMessage);
-			getLogger().info(String.format("Now watching: %s when entity chunk count above %s", allowedEntityList.toString(), entitySpawnCap));
+
+			new BreederListener(this, entitySpawnCap, allowedEntityList,
+					breedingFailMessage);
+			getLogger()
+					.info(String
+							.format("Now watching: %s when entity chunk count above %s",
+									allowedEntityList.toString(),
+									entitySpawnCap));
 		}
-		
+
 	}
-	
+
 	@Override
-	public void onDisable()
-	{
+	public void onDisable() {
 		CreatureSpawnEvent.getHandlerList().unregister(this);
 		PlayerInteractEntityEvent.getHandlerList().unregister(this);
 		getLogger().info("ChunkBreedLimit unloaded!");
